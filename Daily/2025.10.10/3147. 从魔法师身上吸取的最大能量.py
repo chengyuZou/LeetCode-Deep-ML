f2e5dc3bdd35d5ec14067c3361a59c939e171a4d
Litar <3265156700@qq.com>
2025-10-13 14:09:30 +0800

    Delete Daily directory

--- Daily/2025.10.10/3147. 从魔法师身上吸取的最大能量.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maximumEnergy(self, energy: List[int], k: int) -> int:
-        ans = 0
-        n = len(energy)
-        f = [0] * (n + k)
-        for i in range(k , n + k):
-            f[i] = energy[i - k] + max(f[i - k] , 0)
-
-        return max(f[-k:])
--- Daily/2025.10.12/2745. 构造最长的新字符串.py
+++
@@ -1,6 +0,0 @@
-# https://leetcode.cn/problems/construct-the-longest-new-string/description/
-
-class Solution:
-    def longestString(self, x: int, y: int, z: int) -> int:
-        value = 2 * z + min(x , y) * 4
-        return value + 2 if x != y else value 

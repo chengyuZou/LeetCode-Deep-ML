eb3be75684b847d24a299b4e11876605db37056c
Litar <3265156700@qq.com>
2025-10-17 10:06:50 +0800

    Create 912. 排序数组.py

---
+++ LeetCode/重点题型/排序/912. 排序数组/912. 排序数组.py
@@ -0,0 +1,80 @@
+# https://leetcode.cn/problems/sort-an-array/description/
+# 参考https://leetcode.cn/problems/sort-an-array/solutions/3799866/onlogn-kuai-su-pai-xu-fen-zhi-xie-fa-you-njpq/
+class Solution:
+    def partition(self, nums: List[int], left: int, right: int) -> int:
+        """
+        在子数组 [left, right] 中随机选择一个基准元素 pivot
+        根据 pivot 重新排列子数组 [left, right]
+        重新排列后，<= pivot 的元素都在 pivot 的左侧，>= pivot 的元素都在 pivot 的右侧
+        返回 pivot 在重新排列后的 nums 中的下标
+        特别地，如果子数组的所有元素都等于 pivot，我们会返回子数组的中心下标，避免退化
+        """
+
+        # 1. 在子数组 [left, right] 中随机选择一个基准元素 pivot
+        i = randint(left, right)
+        pivot = nums[i]
+        # 把 pivot 与子数组第一个元素交换，避免 pivot 干扰后续划分，从而简化实现逻辑
+        nums[i], nums[left] = nums[left], nums[i]
+
+        # 2. 相向双指针遍历子数组 [left + 1, right]
+        # 循环不变量：在循环过程中，子数组的数据分布始终如下图
+        # [ pivot | <=pivot | 尚未遍历 | >=pivot ]
+        #   ^                 ^     ^         ^
+        #   left              i     j         right
+
+        i, j = left + 1, right
+        while True:
+            while i <= j and nums[i] < pivot:
+                i += 1
+            # 此时 nums[i] >= pivot
+
+            while i <= j and nums[j] > pivot:
+                j -= 1
+            # 此时 nums[j] <= pivot
+
+            if i >= j:
+                break
+
+            # 维持循环不变量
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+
+        # 循环结束后
+        # [ pivot | <=pivot | >=pivot ]
+        #   ^             ^   ^     ^
+        #   left          j   i     right
+
+        # 3. 把 pivot 与 nums[j] 交换，完成划分（partition）
+        # 为什么与 j 交换？
+        # 如果与 i 交换，可能会出现 i = right + 1 的情况，已经下标越界了，无法交换
+        # 而与 j 交换，即使 j = left，交换也不会出错
+        nums[left], nums[j] = nums[j], nums[left]
+
+        # 交换后
+        # [ <=pivot | pivot | >=pivot ]
+        #               ^
+        #               j
+
+        # 返回 pivot 的下标
+        return j
+
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # 快速排序子数组 [left, right]
+        def quick_sort(left: int, right: int) -> None:
+            # 优化：如果子数组已是升序，直接返回
+            # 也可以写 if all(nums[i] <= nums[i + 1] for i in range(left, right)): return
+            ordered = True
+            for i in range(left, right):
+                if nums[i] > nums[i + 1]:
+                    ordered = False
+                    break
+            if ordered:
+                return
+
+            i = self.partition(nums, left, right)  # 划分子数组
+            quick_sort(left, i - 1)   # 排序在 pivot 左侧的元素
+            quick_sort(i + 1, right)  # 排序在 pivot 右侧的元素
+
+        quick_sort(0, len(nums) - 1)
+        return nums
